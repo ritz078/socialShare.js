71100e5fbebab68edfe072072e6855480ae492d0
ritz078 <rkritesh078@gmail.com>
2015-03-31 04:08:30 +0530

    feat:a single http call to get all share data with backup available

--- src/jquery.social.js
+++ src/jquery.social.js
@@ -24,7 +24,30 @@
     return this.each(function () {
       var process = {
 
+        all: {
+          /**
+           * The main API to get the counts of all the Social Sites
+           */
+          getCount: function (opts) {
+            var deferred = jQuery.Deferred();
+            jQuery.getJSON('https://count.donreach.com/?url=' + encodeURIComponent(opts.url) + '&callback=?',
+              function (data) {
+                deferred.resolve(data);
+              });
+            return deferred.promise();
+          }
+        },
+
+
         twitter: {
+
+          /**
+           * Get The count of twitter shares.
+           * Used only when the main API has some issues.
+           * @param opts
+           * @returns {*} promise when the data is resolved
+           */
+
           getCount: function (opts) {
             var deferred = jQuery.Deferred();
             jQuery.getJSON('http://cdn.api.twitter.com/1/urls/count.json?url=' + opts.url + '&callback=?',
@@ -38,6 +61,14 @@
         },
 
         facebook: {
+
+          /**
+           * Get The count of facebook shares.
+           * Used only when the main API has some issues.
+           * @param opts
+           * @returns {*} promise when the data is resolved
+           */
+
           getCount: function (opts) {
             var deferred = jQuery.Deferred();
             jQuery.getJSON('http://graph.facebook.com/?id=' + opts.url + '&callback=?',
@@ -50,6 +81,14 @@
         },
 
         pinterest: {
+
+          /**
+           * Get The count of pinterest shares.
+           * Used only when the main API has some issues.
+           * @param opts
+           * @returns {*} promise when the data is resolved
+           */
+
           getCount: function (opts) {
             var deferred = jQuery.Deferred();
             jQuery.getJSON('http://api.pinterest.com/v1/urls/count.json?callback=?&url=' + opts.url,
@@ -62,6 +101,14 @@
         },
 
         linkedin: {
+
+          /**
+           * Get The count of linkedin shares.
+           * Used only when the main API has some issues.
+           * @param opts
+           * @returns {*} promise when the data is resolved
+           */
+
           getCount: function (opts) {
             var deferred = jQuery.Deferred();
             jQuery.getJSON('http://www.linkedin.com/countserv/count/share?url=' + opts.url + '&format=jsonp&callback=?',
@@ -74,6 +121,15 @@
         },
 
         googleplus: {
+
+          /**
+           * Get The count of G+ shares.
+           * Used only when the main API has some issues.
+           * This is a workaround as default api doesn't work without Authentication key.
+           * @param opts
+           * @returns {*} promise when the data is resolved
+           */
+
           getCount: function (opts) {
             var deferred = jQuery.Deferred();
             jQuery.getJSON('http://anyorigin.com/get?callback=?&url=' + encodeURIComponent('https://plusone.google.com/_/+1/fastbutton?url=' + opts.url),
@@ -87,7 +143,7 @@
         }
       };
 
-      process.googleplus.getCount(options).then(function (d) {
+      process.all.getCount(options).then(function (d) {
         console.log(d);
       });
     });
