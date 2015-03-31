6df72667bcf621bda9ba90a106c85ccc5e982fb3
ritz078 <rkritesh078@gmail.com>
2015-03-31 22:07:37 +0530

    feat: share views now rendering

--- src/jquery.social.js
+++ src/jquery.social.js
@@ -11,17 +11,20 @@
 
     var defaultOptions = {
       url        : window.location.href,
-      description: jQuery('meta[name=description]').attr("content"),
-      title      : jQuery('title').text(),
+      description: $('meta[name=description]').attr("content"),
+      title      : $('title').text(),
       twitter    : true,
       facebook   : true,
       pinterest  : true,
-      linkedIn   : true
+      linkedIn   : true,
+      gPlus      : true
     };
+
+    var that=this;
 
     options = jQuery.extend({}, defaultOptions, options);
 
-    return this.each(function () {
+    return this.each(function (i) {
       var process = {
 
         all: {
@@ -30,14 +33,26 @@
            */
           getCount: function (opts) {
             var deferred = jQuery.Deferred();
+            var counts = {};
             jQuery.getJSON('https://count.donreach.com/?url=' + encodeURIComponent(opts.url) + '&callback=?',
               function (data) {
-                deferred.resolve(data);
+                counts.url = data.url;
+                counts.facebook = data.shares.facebook;
+                counts.twitter = data.shares.twitter;
+                counts.linkedIn = data.shares.linkedin;
+                counts.pinterest = data.shares.pinterest;
+                counts.gPlus = data.shares.google;
+                counts.success = true;
+                deferred.resolve(counts);
+              })
+              .fail(function (jqxhr, textStatus, error) {
+                alert(error);
+                counts.success = false;
+                deferred.resolve(counts);
               });
             return deferred.promise();
           }
         },
-
 
         twitter: {
 
@@ -48,12 +63,12 @@
            * @returns {*} promise when the data is resolved
            */
 
-          getCount: function (opts) {
+          getCount: function (opts, counts) {
             var deferred = jQuery.Deferred();
             jQuery.getJSON('http://cdn.api.twitter.com/1/urls/count.json?url=' + opts.url + '&callback=?',
               function (data) {
-                data.service = "twitter";
-                deferred.resolve(data);
+                counts.twitter = data.count;
+                deferred.resolve(counts);
               });
             return deferred.promise();
           }
@@ -69,12 +84,12 @@
            * @returns {*} promise when the data is resolved
            */
 
-          getCount: function (opts) {
+          getCount: function (opts, counts) {
             var deferred = jQuery.Deferred();
-            jQuery.getJSON('http://graph.facebook.com/?id=' + opts.url + '&callback=?',
+            jQuery.getJSON('https://api.facebook.com/method/links.getStats?format=json&urls=' + opts.url + '&callback=?',
               function (data) {
-                data.service = "facebook";
-                deferred.resolve(data);
+                counts.facebook = data[0].total_count;
+                deferred.resolve(counts);
               });
             return deferred.promise();
           }
@@ -89,12 +104,12 @@
            * @returns {*} promise when the data is resolved
            */
 
-          getCount: function (opts) {
+          getCount: function (opts, counts) {
             var deferred = jQuery.Deferred();
             jQuery.getJSON('http://api.pinterest.com/v1/urls/count.json?callback=?&url=' + opts.url,
               function (data) {
-                data.service = "pinterest";
-                deferred.resolve(data);
+                counts.pinterest = data.count;
+                deferred.resolve(counts);
               });
             return deferred.promise();
           }
@@ -109,12 +124,12 @@
            * @returns {*} promise when the data is resolved
            */
 
-          getCount: function (opts) {
+          getCount: function (opts, counts) {
             var deferred = jQuery.Deferred();
-            jQuery.getJSON('http://www.linkedin.com/countserv/count/share?url=' + opts.url + '&format=jsonp&callback=?',
+            jQuery.getJSON('https://www.linkedin.com/countserv/count/share?format=jsonp&url=' + opts.url + '&callback=?',
               function (data) {
-                data.service = "linkedin";
-                deferred.resolve(data);
+                counts.linkedIn = data.count;
+                deferred.resolve(counts);
               });
             return deferred.promise();
           }
@@ -130,22 +145,70 @@
            * @returns {*} promise when the data is resolved
            */
 
-          getCount: function (opts) {
+          getCount: function (opts, counts) {
             var deferred = jQuery.Deferred();
             jQuery.getJSON('http://anyorigin.com/get?callback=?&url=' + encodeURIComponent('https://plusone.google.com/_/+1/fastbutton?url=' + opts.url),
               function (data) {
-                data.service = "googleplus";
-                data.count = data.contents.match(/\{c: (\d+)/)[1];
-                deferred.resolve(data);
+                counts.gPlus = parseFloat(data.contents.match(/\{c: [0-9A-Z.]+/)[0].split(' ')[1]);
+                deferred.resolve(counts);
               });
             return deferred.promise();
           }
         }
       };
 
+      var counts = {};
+
       process.all.getCount(options).then(function (d) {
-        console.log(d);
+        counts = d;
+
+        if (!d.success) {
+          $.when(
+            (options.facebook ? process.facebook.getCount(options, counts) : null),
+            (options.twitter ? process.twitter.getCount(options, counts) : null),
+            (options.pinterest ? process.pinterest.getCount(options, counts) : null),
+            (options.linkedIn ? process.linkedIn.getCount(options, counts) : null),
+            (options.gPlus ? process.gPlus.getCount(options, counts) : null)
+          )
+            .then(function () {
+              render(counts);
+            });
+        }
+        else{
+          render(counts);
+        }
+
       });
+
+      function render(c){
+        if(options.facebook){
+          that.find('.facebook-count').each(function(){
+            $(this).html(c.facebook);
+          })
+        }
+        if(options.twitter){
+          that.find('.twitter-count').each(function(){
+            $(this).html(c.twitter);
+          })
+        }
+        if(options.pinterest){
+          that.find('.pinterest-count').each(function(){
+            $(this).html(c.pinterest);
+          })
+        }
+        if(options.linkedIn){
+          that.find('.linkedin-count').each(function(){
+            $(this).html(c.linkedIn);
+          })
+        }
+        if(options.gPlus){
+          that.find('.google-plus-count').each(function(){
+            $(this).html(c.gPlus);
+          })
+        }
+      }
+
+
     });
   };
 }(jQuery));
